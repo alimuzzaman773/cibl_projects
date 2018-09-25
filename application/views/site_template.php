cac6943aba9a0a32827ddf69c877ee3caada2eae
Shahin Khalifa <khalifa.shahin@gmail.com>
2018-09-25 16:29:50 +0600

    premier live deploy

--- application/views/site_template.php
+++ application/views/site_template.php
@@ -197,7 +197,7 @@
         <footer>
             <div class="container">
                 <hr />
-                <p class="text-center">&copy; PREMIER PLUS</p>
+                <p class="text-center">&copy; PREMIER BANK</p>
             </div>
         </footer>
     </body>
