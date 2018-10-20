591f2bc45dd273d9c3075291319af52f33485b67
arif_ <arif_@Arif-Asus>
2018-10-20 14:33:07 +0600

    migration for username 2 added

---
+++ migration_2018-10-20.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `apps_users` ADD `userName2` VARCHAR(255) NULL DEFAULT NULL AFTER `homeBranchCode`, ADD UNIQUE (`userName2`);
+ALTER TABLE `apps_users_mc` ADD `userName2` VARCHAR(256) NULL DEFAULT NULL AFTER `homeBranchCode`, ADD UNIQUE (`userName2`);
