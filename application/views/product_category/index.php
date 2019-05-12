f5d458197ff54399da13e85b02cee1c9f8e3a5d7
Shahid <cse.shahid@gmail.com>
2019-05-12 11:16:43 +0600

    Product category

--- application/views/product_category/index.php
+++ application/views/product_category/index.php
@@ -2,45 +2,55 @@
 <?= $output ?>
 <script>
     var app = app || {};
-    app.crudState = '<?=$crudState?>';
+    app.crudState = '<?= $crudState ?>';
 
     app.categories = <?= json_encode($categories) ?>;
-    app.categoryInfo = <?= json_encode($categoryInfo)?>;
-    
-    app.getOptions = function(type, categories) {
+    app.categoryInfo = <?= json_encode($categoryInfo) ?>;
+
+    app.getOptions = function (type, categories) {
         var $html = '<option></option>';
-        $.each(categories, function(i, v){
-            if (type !== undefined && type == v.type){
+        $.each(categories, function (i, v) {
+            if (type !== undefined && type == v.type) {
                 $html += '<option value="' + v.pc_id + '">' + v.name + '</option>';
             }
         });
         return $html;
-    }
+    };
 
-    $(document).ready(function(e){
-        if(app.crudState == 'add' || app.crudState == 'edit'){
+    $(document).ready(function (e) {
+        if (app.crudState == 'add' || app.crudState == 'edit') {
             //var $optionHtml = app.getOptions($('#field-type').val(), app.categories);
             //$('#field-parent_id').html($optionHtml);
-            if(app.crudState == 'add'){
+            if (app.crudState == 'add') {
                 var $optionHtml = app.getOptions($(this).val(), app.categories);
                 //$("#field-parent_id").chosen('destroy');
                 $('#field-parent_id').html($optionHtml);
             }
-            $("#field-type").on('change', function(e){
+            $("#field-type").on('change', function (e) {
                 var $optionHtml = app.getOptions($(this).val(), app.categories);
                 //$("#field-parent_id").chosen('destroy');
                 $('#field-parent_id').html($optionHtml);
-               // $("#field-parent_id").chosen();
-            });        
-            
-            if(app.crudState == 'edit'){
+                // $("#field-parent_id").chosen();
+            });
+
+            if (app.crudState == 'edit') {
                 //$("#field-parent_id").chosen('destroy');
                 //$('#field-parent_id').val(app.categoryInfo.parent_id);
                 //$("#field-parent_id").chosen();
             }
-            
+
         }
-        
-        
+
+        $('#field-type').change(function () {
+            var val = $('#field-type option:selected').val();
+
+            //console.log(val);
+
+            if (val == "product") {
+                $('#parent_id_field_box').show();
+            } else {
+                $('#parent_id_field_box').hide();
+            }
+        });
     });
 </script>
