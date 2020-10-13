f316d290545141e9aaa8c69049deca21e79d3ea3
Shahid <cse.shahid@gmail.com>
2020-10-13 13:45:19 +0600

    index

--- application/views/settings/index.php
+++ application/views/settings/index.php
@@ -59,10 +59,73 @@
                     <small class=''>Time should be minutes</small>
                 </td>
             </tr>
+        </tbody>
+        <thead>
+            <tr class="bg-primary">
+                <th colspan="2">
+                    BEFTN Fund Transfer
+                    <?php
+                    $settings['beftn'] = (isset($settings['beftn']) ? $settings['beftn'] : array());
+                    ?>
+                </th>
+            </tr>        
+        </thead>
+        <tbody>
             <tr>
                 <td><b>BEFTN Date</b></td>
                 <td>
                     <input type="text" class="form-control input-sm" name="beftn[beftn_date]" value="<?= $this->settings_model->getItem('beftn_date', $settings['beftn']) ?>" id="beftnDate" />                
+                </td>
+            </tr>
+        </tbody>
+        <thead>
+            <tr class="bg-primary">
+                <th colspan="2">
+                    RTGS Fund Transfer
+                    <?php
+                    $settings['rtgs_ft'] = (isset($settings['rtgs_ft']) ? $settings['rtgs_ft'] : array());
+                    ?>
+                </th>
+            </tr>        
+        </thead>
+        <tbody>
+            <tr>    
+                <td><b>Maximum Amount Per Transaction</b></td>
+                <td>
+                    <input type="number" min="0" class='form-control' name="rtgs_ft[rtgs_max]" value="<?= (int) $this->settings_model->getItem('rtgs_max', $settings['rtgs_ft']) ?>" />
+                </td>
+            </tr>
+            <tr>    
+                <td><b>Minimum Amount Per Transaction</b></td>
+                <td>
+                    <input type="number" min="0" class='form-control' name="rtgs_ft[rtgs_min]" value="<?= (int) $this->settings_model->getItem('rtgs_min', $settings['rtgs_ft']) ?>" />
+                </td>
+            </tr>
+            <tr>    
+                <td><b>Charge on RTGS</b></td>
+                <td>
+                    <input type="number" min="0" class='form-control' name="rtgs_ft[rtgs_charge]" value="<?= (int) $this->settings_model->getItem('rtgs_charge', $settings['rtgs_ft']) ?>" />
+                </td>
+            </tr>
+            <tr>    
+                <td><b>VAT on Charge</b></td>
+                <td>
+                    <input type="number" min="0" class='form-control' name="rtgs_ft[rtgs_vat_of_charge]" value="<?= (int) $this->settings_model->getItem('rtgs_vat_of_charge', $settings['rtgs_ft']) ?>" />
+                </td>
+            </tr>
+            <tr>    
+                <td><b>RTGS Transaction Time</b></td>
+                <td>
+                    <label for="from_time">Transaction Time</label>
+                    <input type="time" id="from_time" name="rtgs_ft[rtgs_from_time]" value="<?= $this->settings_model->getItem('rtgs_from_time', $settings['rtgs_ft']) ?>">
+                    <label for="to_time">To</label>
+                    <input type="time" id="to_time" name="rtgs_ft[rtgs_to_time]" value="<?= $this->settings_model->getItem('rtgs_to_time', $settings['rtgs_ft']) ?>">
+                </td>
+            </tr>
+            <tr>    
+                <td><b>RTGS Message</b></td>
+                <td>
+                    <textarea class="form-control" name="rtgs_ft[rtgs_text]" rows="4" cols="50"><?= $this->settings_model->getItem('rtgs_text', $settings['rtgs_ft']) ?></textarea>
                 </td>
             </tr>
         </tbody>
@@ -79,55 +142,55 @@
 
 <script type="text/javascript" src="<?= base_url() . ASSETS_FOLDER ?>js/jquery.validate.min.js"></script>
 <script type="text/javascript">
-    var saveData = function ($this) {
-        if (!$("#settingsform").valid()) {
-            return false;
-        }
+                    var saveData = function ($this) {
+                        if (!$("#settingsform").valid()) {
+                            return false;
+                        }
 
-        $btnhtml = $($this).html();
-        $($this).attr("disabled", "disabled");
-        $($this).html("Saving...");
-        var formData = $("#settingsform").serialize();
-        $.ajax({
-            url: app.baseUrl + "settings/save_settings",
-            data: formData,
-            type: 'post',
-            dataType: 'json',
-            success: function (data) {
-                $($this).html($btnhtml);
-                $($this).removeAttr("disabled");
+                        $btnhtml = $($this).html();
+                        $($this).attr("disabled", "disabled");
+                        $($this).html("Saving...");
+                        var formData = $("#settingsform").serialize();
+                        $.ajax({
+                            url: app.baseUrl + "settings/save_settings",
+                            data: formData,
+                            type: 'post',
+                            dataType: 'json',
+                            success: function (data) {
+                                $($this).html($btnhtml);
+                                $($this).removeAttr("disabled");
 
-                if (data.success) {
-                    alert("Data has been saved");
-                    window.location = app.baseUrl+"settings";
-                    return false;
-                }
+                                if (data.success) {
+                                    alert("Data has been saved");
+                                    window.location = app.baseUrl + "settings";
+                                    return false;
+                                }
 
-                alert(data.msg);
-            },
-            error: function (data) {
-                $($this).html($btnhtml);
-                $($this).removeAttr("disabled");
-                alert("There was a problem. Please try again later.");
-            }
-        });
+                                alert(data.msg);
+                            },
+                            error: function (data) {
+                                $($this).html($btnhtml);
+                                $($this).removeAttr("disabled");
+                                alert("There was a problem. Please try again later.");
+                            }
+                        });
 
-        return false;
-    };
+                        return false;
+                    };
 
-    $(document).ready(function () {
-        $.validator.addMethod("time", function (value, element) {
-            return this.optional(element) || /^(([0-1]?[0-9])|([2][0-3])):([0-5]?[0-9])(:([0-5]?[0-9]))?$/i.test(value);
-        }, "Please enter a valid time. Format - 10:25");
-        $("#settingsform").validate();
-        
-        
-        $("#beftnDate").datepicker({yearRange:'c-10:c+10', dateFormat:'dd-MM-yy', changeMonth:true, changeYear:true}).on('focusin', function(){
-        //$(this).prop("readonly", true);
-        }).on('focusout', function(){
-        //$(this).prop("readonly", false);
-        });
-        
+                    $(document).ready(function () {
+                        $.validator.addMethod("time", function (value, element) {
+                            return this.optional(element) || /^(([0-1]?[0-9])|([2][0-3])):([0-5]?[0-9])(:([0-5]?[0-9]))?$/i.test(value);
+                        }, "Please enter a valid time. Format - 10:25");
+                        $("#settingsform").validate();
 
-    });
+
+                        $("#beftnDate").datepicker({yearRange: 'c-10:c+10', dateFormat: 'dd-MM-yy', changeMonth: true, changeYear: true}).on('focusin', function () {
+                            //$(this).prop("readonly", true);
+                        }).on('focusout', function () {
+                            //$(this).prop("readonly", false);
+                        });
+
+
+                    });
 </script>
